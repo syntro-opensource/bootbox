8b43fbfd922c7d73c8cd444018a77657140b6709
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-07-23 21:33:25 +0200

    ADD: Ripple effect

---
+++ js/ripple.js
@@ -0,0 +1,20 @@
+var addRippleEffect = function(press) {
+	var target = press.target;
+	if (!target.classList.contains('effect-ripple')) return false;
+	var rect = target.getBoundingClientRect();
+	var ripple = target.querySelector('.ripple');
+	if (!ripple) {
+		ripple = document.createElement('span');
+		ripple.className = 'ripple';
+		ripple.style.height = ripple.style.width = Math.max(press.target.offsetWidth, target.offsetHeight) + 'px';
+		target.appendChild(ripple);
+	}
+	ripple.classList.remove('show');
+	var top = press.pageY - rect.top - ripple.offsetHeight / 2 - window.scrollY;
+	var left = press.pageX - rect.left - ripple.offsetWidth / 2 - window.scrollX;
+	ripple.style.top = top + 'px';
+	ripple.style.left = left + 'px';
+	ripple.classList.add('show');
+	return false;
+}
+document.addEventListener('click', addRippleEffect, false);
